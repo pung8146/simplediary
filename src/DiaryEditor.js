b93d3594431b9272080a5b20b6cd2cf9733885e1
pung8146 <pung8146@naver.com>
2023-01-31 23:36:36 +0900

    같은 형태의 state와 함수는 묶어서 객체 형식으로도 사용 할 수 있습니다.

--- src/DiaryEditor.js
+++ src/DiaryEditor.js
@@ -3,22 +3,45 @@ import React,{useState} from 'react'
 
 
 export default function DiaryEditor () {
-  const [author, setAuthor] = useState('');
-    const [content,setContent] = useState('')
+  
+  const [state,setState] = useState({
+    author: "",
+    content: "",
+    emotion: 1,
+  })
 
+  const handleChangeState = (e) => {
+    setState({
+      ...state,
+      [e.target.name]:e.target.value, 
+    })
+  }
+
+  const handleSubmit = () => {
+    console.log(state)
+    alert(state)
+  }
     return (
     <div className='DiaryEditor'>
         <h2>오늘의 일기</h2>
         <input 
-        name='author '
-        value={author} onChange={(e)=>{
-            console.log(e.target.value)
-            setAuthor(e.target.value)
-        }}/>
+        name='author'
+        value={state.author}
+        onChange={handleChangeState}/>
         <div>
-            <textarea value={content} onChange={(e)=>{
-                setContent(e.target.value)
-            }}/>
+            <textarea name="content" value={state.content} onChange={handleChangeState}/>
+        </div>
+        <div>
+          <select name="emotion" value={state.emotion} onChange={handleChangeState}>
+            <option value={1}>1</option>
+            <option value={2}>2</option>
+            <option value={3}>3</option>
+            <option value={4}>4</option>
+            <option value={5}>5</option>
+          </select>
+        </div>
+        <div>
+          <button onClick={handleSubmit}>일기 저장하기</button>
         </div>
     </div>
   )
