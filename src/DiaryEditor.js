2457987d8b20ff4287eec77e2ab0e4b9a7db849e
pung8146 <pung8146@naver.com>
2023-02-06 14:07:32 +0900

    HTML DOM 요소 접근할 수 있는 useRef 기능을 사용했습니다.

--- src/DiaryEditor.js
+++ src/DiaryEditor.js
@@ -1,9 +1,10 @@
-import React,{useState} from 'react'
+import React,{useRef, useState} from 'react'
 
 
 
 export default function DiaryEditor () {
-  
+  const authorInput = useRef();  
+  const contentInput = useRef();
   const [state,setState] = useState({
     author: "",
     content: "",
@@ -18,18 +19,28 @@ export default function DiaryEditor () {
   }
 
   const handleSubmit = () => {
-    console.log(state)
-    alert(state)
+    if(state.author.length < 1) {
+      authorInput.current.foucs();
+      return;
+    }
+
+    if(state.content.length < 5) {
+      alert('최소 다섯글자 이상 입력해주세요')
+      contentInput.current.focus();
+      return;
+    }
+    alert('저장 성공!')
   }
     return (
     <div className='DiaryEditor'>
         <h2>오늘의 일기</h2>
-        <input 
+        <input
+        ref={authorInput} 
         name='author'
         value={state.author}
         onChange={handleChangeState}/>
         <div>
-            <textarea name="content" value={state.content} onChange={handleChangeState}/>
+            <textarea ref={contentInput} name="content" value={state.content} onChange={handleChangeState}/>
         </div>
         <div>
           <select name="emotion" value={state.emotion} onChange={handleChangeState}>
