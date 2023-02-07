3e73c8bf71e5be1cc0a6fbe35fdac343c00dd2ef
pung8146 <pung8146@naver.com>
2023-02-07 19:19:53 +0900

    수정하기 기능 추가 / Props변경시 하위 컴포넌트에 모두 변경해줘야함

--- src/DiaryItem.js
+++ src/DiaryItem.js
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const DiaryItem = ({onDelete,author, content, created_date, emotion, id}) => {
+const DiaryItem = ({onRemove,author, content, created_date, emotion, id}) => {
+
+  const [isEdit,setIsEdit] = useState(false)
+  const toggleIsEdit = () => setIsEdit(!isEdit)
+
+  const [localContent,setLocalContent] = useState("");
+
+  const handleRemove = () => {
+    if(window.confirm(`${id}번째 일기를 정말 삭제할까요?`)){
+      onRemove(id)
+  }
+  }
+
     return (
         <div className="DiaryItem">
         <div className="info">
@@ -10,17 +22,18 @@ const DiaryItem = ({onDelete,author, content, created_date, emotion, id}) => {
           <br />
           <span className="date">{new Date(created_date).toLocaleString()}</span>
         </div>
-        <div className="content">{content}</div>
+        <div className="content">{isEdit ? <><textarea value={localContent} onChange={(e) => setLocalContent(e.target.value)}/></> : <>{content}</>}</div>
+        {isEdit ? <button
+            onClick={handleRemove}
+        > 삭제하기</button> 
+        :
+        <button onClick={toggleIsEdit}>수정하기</button> }
         <button
-            onClick={()=>{
-            console.log(id)
-            if(window.confirm(`${id}번째 일기를 정말 삭제할까요?`)){
-                onDelete(id)
-            }
-        }}
+            onClick={handleRemove}
         >
           삭제하기
         </button>
+        <button onClick={toggleIsEdit}>수정하기</button>
       </div>
     );
 };
