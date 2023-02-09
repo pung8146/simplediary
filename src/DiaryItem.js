958fe8962c478fbb98cfd17b04dff7787da6ee19
pung8146 <pung8146@naver.com>
2023-02-09 14:31:18 +0900

    onEdit 과 editHandle 을 만들어서 수정 완성과 취소를 구현

--- src/DiaryItem.js
+++ src/DiaryItem.js
@@ -1,21 +1,39 @@
-import React, { useState } from "react";
+import { useRef, useState } from "react";
 
 const DiaryItem = ({
   onRemove,
+  onEdit,
+  id,
   author,
   content,
-  created_date,
   emotion,
-  id,
+  created_date
 }) => {
+  const localContentInput = useRef();
+  const [localContent, setLocalContent] = useState(content);
   const [isEdit, setIsEdit] = useState(false);
   const toggleIsEdit = () => setIsEdit(!isEdit);
 
-  const [localContent, setLocalContent] = useState("");
-
-  const handleRemove = () => {
-    if (window.confirm(`${id}번째 일기를 정말 삭제할까요?`)) {
+  const handleClickRemove = () => {
+    if (window.confirm(`${id}번째 일기를 정말 삭제하시겠습니까?`)) {
       onRemove(id);
+    }
+  };
+
+  const handleQuitEdit = () => {
+    setIsEdit(false);
+    setLocalContent(content);
+  };
+
+  const handleEdit = () => {
+    if (localContent.length < 5) {
+      localContentInput.current.focus();
+      return;
+    }
+
+    if (window.confirm(`${id}번 째 일기를 수정하시겠습니까?`)) {
+      onEdit(id, localContent);
+      toggleIsEdit();
     }
   };
 
@@ -23,32 +41,36 @@ const DiaryItem = ({
     <div className="DiaryItem">
       <div className="info">
         <span className="author_info">
-          작성자 : {author} | 감정점수 : {emotion}
+          작성자 : {author} | 감정 : {emotion}
         </span>
         <br />
-        <span className="date">{new Date(created_date).toLocaleString()}</span>
+        <span className="date">
+          {new Date(created_date).toLocaleDateString()}
+        </span>
       </div>
       <div className="content">
         {isEdit ? (
-          <>
-            <textarea
-              value={localContent}
-              onChange={(e) => setLocalContent(e.target.value)}
-            />
-          </>
+          <textarea
+            ref={localContentInput}
+            value={localContent}
+            onChange={(e) => setLocalContent(e.target.value)}
+          />
         ) : (
-          <>{content}</>
+          content
         )}
       </div>
       {isEdit ? (
-        <button onClick={handleRemove}> 삭제하기</button>
+        <>
+          <button onClick={handleQuitEdit}>수정 취소</button>
+          <button onClick={handleEdit}>수정 완료</button>
+        </>
       ) : (
-        <button onClick={toggleIsEdit}>수정하기</button>
+        <>
+          <button onClick={handleClickRemove}>삭제하기</button>
+          <button onClick={toggleIsEdit}>수정하기</button>
+        </>
       )}
-      <button onClick={handleRemove}>삭제하기</button>
-      <button onClick={toggleIsEdit}>수정하기</button>
     </div>
   );
 };
-
 export default DiaryItem;
