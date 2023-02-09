68d38ff42bf2f013962e38fb813cf5a35524117c
pung8146 <pung8146@naver.com>
2023-02-09 10:47:43 +0900

    setting prettier 변경

--- src/App.js
+++ src/App.js
@@ -1,68 +1,67 @@
-import { useRef, useState } from 'react';
-import './App.css';
-import DiaryEditor from './DiaryEditor';
-import DiaryList from './DiaryList';
+import { useRef, useState } from "react";
+import "./App.css";
+import DiaryEditor from "./DiaryEditor";
+import DiaryList from "./DiaryList";
 
 // const dummyList = [
-  // {
-  //   id:1,
-  //   author:"홍길동",
-  //   content:"의로운 의적",
-  //   created_date: new Date().getTime(),
-  //   emotion: 1,
-  // },
-  // {
-  //   id:2,
-  //   author:"이순신",
-  //   content:"멋진 장군님",
-  //   created_date: new Date().getTime(),
-  //   emotion: 2,
-  // },{
-  //   id:3,
-  //   author:"삼방울",
-  //   content:"상방울 자매",
-  //   created_date: new Date().getTime(),
-  //   emotion: 3,
-  // },
-  // {
-  //   id:4,
-  //   author:"홍길동4",
-  //     content:"의로운 의적",
-  //   created_date: new Date().getTime(),
-  //   emotion: 1,
-  // }, 
+// {
+//   id:1,
+//   author:"홍길동",
+//   content:"의로운 의적",
+//   created_date: new Date().getTime(),
+//   emotion: 1,
+// },
+// {
+//   id:2,
+//   author:"이순신",
+//   content:"멋진 장군님",
+//   created_date: new Date().getTime(),
+//   emotion: 2,
+// },{
+//   id:3,
+//   author:"삼방울",
+//   content:"상방울 자매",
+//   created_date: new Date().getTime(),
+//   emotion: 3,
+// },
+// {
+//   id:4,
+//   author:"홍길동4",
+//     content:"의로운 의적",
+//   created_date: new Date().getTime(),
+//   emotion: 1,
+// },
 // ]
 
 function App() {
+  const [data, setData] = useState([]);
 
-  const [data,setData] = useState([]);
-
-  const dataId = useRef(0)
+  const dataId = useRef(0);
 
   const onCreate = (author, emotion, content) => {
-    const created_date = new Date().getTime(); 
+    const created_date = new Date().getTime();
     const newItem = {
       author,
       content,
       emotion,
       created_date,
       id: dataId.current,
-     }
-     dataId.current += 1;
-     setData([newItem,...data])
-  }
+    };
+    dataId.current += 1;
+    setData([newItem, ...data]);
+  };
 
   const onRemove = (targetId) => {
-    console.log(`${targetId}가 삭제 되었습니다.`)
+    console.log(`${targetId}가 삭제 되었습니다.`);
     const newDiaryList = data.filter((it) => it.id !== targetId);
-    setData(newDiaryList)
-  }
+    setData(newDiaryList);
+  };
 
   return (
-    <div className="App">  
-      <DiaryEditor onCreate={onCreate}/>
-      <DiaryList onRemove={onRemove} diaryList={data}/>
-      </div>
+    <div className="App">
+      <DiaryEditor onCreate={onCreate} />
+      <DiaryList onRemove={onRemove} diaryList={data} />
+    </div>
   );
 }
 
--- src/DiaryItem.js
+++ src/DiaryItem.js
@@ -1,41 +1,54 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-const DiaryItem = ({onRemove,author, content, created_date, emotion, id}) => {
+const DiaryItem = ({
+  onRemove,
+  author,
+  content,
+  created_date,
+  emotion,
+  id,
+}) => {
+  const [isEdit, setIsEdit] = useState(false);
+  const toggleIsEdit = () => setIsEdit(!isEdit);
 
-  const [isEdit,setIsEdit] = useState(false)
-  const toggleIsEdit = () => setIsEdit(!isEdit)
-
-  const [localContent,setLocalContent] = useState("");
+  const [localContent, setLocalContent] = useState("");
 
   const handleRemove = () => {
-    if(window.confirm(`${id}번째 일기를 정말 삭제할까요?`)){
-      onRemove(id)
-  }
-  }
+    if (window.confirm(`${id}번째 일기를 정말 삭제할까요?`)) {
+      onRemove(id);
+    }
+  };
 
-    return (
-        <div className="DiaryItem">
-        <div className="info">
-          <span className="author_info">
-            작성자 : {author} | 감정점수 : {emotion}
-          </span>
-          <br />
-          <span className="date">{new Date(created_date).toLocaleString()}</span>
-        </div>
-        <div className="content">{isEdit ? <><textarea value={localContent} onChange={(e) => setLocalContent(e.target.value)}/></> : <>{content}</>}</div>
-        {isEdit ? <button
-            onClick={handleRemove}
-        > 삭제하기</button> 
-        :
-        <button onClick={toggleIsEdit}>수정하기</button> }
-        <button
-            onClick={handleRemove}
-        >
-          삭제하기
-        </button>
-        <button onClick={toggleIsEdit}>수정하기</button>
+  return (
+    <div className="DiaryItem">
+      <div className="info">
+        <span className="author_info">
+          작성자 : {author} | 감정점수 : {emotion}
+        </span>
+        <br />
+        <span className="date">{new Date(created_date).toLocaleString()}</span>
       </div>
-    );
+      <div className="content">
+        {isEdit ? (
+          <>
+            <textarea
+              value={localContent}
+              onChange={(e) => setLocalContent(e.target.value)}
+            />
+          </>
+        ) : (
+          <>{content}</>
+        )}
+      </div>
+      {isEdit ? (
+        <button onClick={handleRemove}> 삭제하기</button>
+      ) : (
+        <button onClick={toggleIsEdit}>수정하기</button>
+      )}
+      <button onClick={handleRemove}>삭제하기</button>
+      <button onClick={toggleIsEdit}>수정하기</button>
+    </div>
+  );
 };
 
 export default DiaryItem;
